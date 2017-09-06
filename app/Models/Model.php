ddc01108b6ce0f06b3e4e9f8af03a116bfe2a06b
nevadskiy <nevadskiy@gmail.com>
2017-09-06 19:19:38 +0300

    back

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -1,6 +1,19 @@
 <?php
 
+namespace Acme\Models;
+
 class Model
 {
+    private $db;
 
+    public function __construct()
+    {
+        $this->db = QueryBuilder::new();
+//        $this->checkCookieToken();
+    }
+
+    public function db()
+    {
+        return $this->db;
+    }
 }
